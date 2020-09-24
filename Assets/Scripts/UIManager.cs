c34f1a54e9c63a51f172721c9b4c84eed0d5d6c1
P0l4rb34r311 <71675616+P0l4rb34r311@users.noreply.github.com>
2020-09-23 23:33:37 -0400

    RESTOREPOINT Main Menu updated

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,14 +10,24 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private Text _gameOverText;
     [SerializeField]
+    private Text _restartText;
+    [SerializeField]
     private Image _livesImg;
     [SerializeField]
     private Sprite[] _liveSprites;
+    private GameManager _gameManager;
 
     void Start()
     {
         _scoreText.text = "Score : " + 0;
         _gameOverText.gameObject.SetActive(false);
+        _restartText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
+        
+        if(_gameManager == null)
+        {
+            Debug.LogError("GameManager is NULL");
+        }
     }
 
     // Update is called once per frame
@@ -29,11 +39,18 @@ public class UIManager : MonoBehaviour
     public void UpdateLives(int currentLives)
     {
         _livesImg.sprite = _liveSprites[currentLives];
-        if(currentLives == 0)
+        if (currentLives == 0)
         {
-            _gameOverText.gameObject.SetActive(true);
-            StartCoroutine(GameOverFlickerRoutine());
+            GameOverSequence();
         }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);
+        StartCoroutine(GameOverFlickerRoutine());
     }
 
     IEnumerator GameOverFlickerRoutine()
