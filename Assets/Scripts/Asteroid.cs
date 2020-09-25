08c181dd09de4a082efb17c31b65b082b3a67785
P0l4rb34r311 <71675616+P0l4rb34r311@users.noreply.github.com>
2020-09-25 00:55:46 -0400

    working update to asteriod

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -3,21 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Asteroid : MonoBehaviour
-{
+{    
+    [SerializeField]
+    private float _speed = 0.25f;
     [SerializeField]
     private float _rotateSpeed = 3.0f;
     [SerializeField]
     private GameObject _explosion;
 
+
     void Start()
     {
-        
+        transform.position = new Vector3(0, 8, 0);
     }
 
 
     void Update()
     {
-        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);  
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        transform.position = new Vector3(0, transform.position.y, 0);
+        //transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);
+ 
+        if (transform.position.y < -6f)
+        {
+            transform.position = new Vector3(0, 8, 0);
+        }
+        // Code added to start  asteroid off screen and loop through on a fixed axis til the player shoots it.
+        // gives the appearance of the asteriod moving
     }
     public void OnTriggerEnter2D(Collider2D other)
     {
@@ -26,7 +38,7 @@ public class Asteroid : MonoBehaviour
             Destroy(other.gameObject);
             Instantiate(_explosion, transform.position, Quaternion.identity);
             Destroy(GetComponent<Collider2D>());
-            Destroy(this.gameObject, 0.5f);
+            Destroy(this.gameObject, 0.25f);
         }
     }
 }
