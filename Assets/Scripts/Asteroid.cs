235913de8b5476269a3a0a0a6d4f2a9032c06e49
P0l4rb34r311 <71675616+P0l4rb34r311@users.noreply.github.com>
2020-09-24 22:50:32 -0400

    Update to asteroid

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Asteroid : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private float _rotateSpeed = 3.0f;
+    private Animator _animator;
+
     void Start()
     {
-        
+        _animator = gameObject.GetComponent<Animator>();   
+        if(_animator == null)
+        {
+            Debug.LogError("Animator is NULL");
+        }
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        transform.Rotate(Vector3.forward * _rotateSpeed * Time.deltaTime);  
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Laser")
+        {
+            Destroy(other.gameObject);
+            _animator.SetTrigger("Explosion");
+            Destroy(GetComponent<Collider2D>());
+            Destroy(this.gameObject, 3.0f);
+        }
     }
 }
