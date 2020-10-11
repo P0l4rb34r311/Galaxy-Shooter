0c610b5a1258ba312e8dd8b035b520d48ce0cb43
Clarke <clwagn0894@gmail.com>
2020-10-11 16:37:48 -0400

    UPDATE: small adjustments made

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 3.0f;
+    void Update()
+    {
+        transform.Rotate(Vector3.back * _rotateSpeed * Time.deltaTime, Space.Self);
+    }
+}
