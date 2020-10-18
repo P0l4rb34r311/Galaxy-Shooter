a4f6b9552a2245bf7ff31c0563b8f5d4849cd877
Clarke <clwagn0894@gmail.com>
2020-10-18 14:34:11 -0400

    UPDATE: Player animation fixed

--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -18,21 +18,21 @@ public class PlayerAnimation : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
         {
             _anim.SetBool("TurnLeft", true);
-            _anim.SetBool("TurnRight", false);
+            //_anim.SetBool("TurnRight", false);
         }
         else if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
             _anim.SetBool("TurnLeft", false);
-            _anim.SetBool("TurnRight", false);
+            //_anim.SetBool("TurnRight", false);
         }
         if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
         {
-            _anim.SetBool("TurnLeft", false);
+            //_anim.SetBool("TurnLeft", false);
             _anim.SetBool("TurnRight", true);
         }
         else if (Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow))
         {
-            _anim.SetBool("TurnLeft", false);
+            //_anim.SetBool("TurnLeft", false);
             _anim.SetBool("TurnRight", false);
         }
     }
